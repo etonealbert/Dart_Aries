6dc2c3d70f6402e00801a486b91237709cb3736b
etonealbert <albert.lukmanov99@gmail.com>
2022-04-14 13:51:57 +0300

    first commit

---
+++ dart_ar.dart
@@ -0,0 +1,11 @@
+
+void sort{
+  
+}
+void main() {
+  List<int> list = [7, 2, 1, 8, 6, 3, 5, 4];
+  int high = list.length - 1;
+  int low = 0;
+  List<int> result = sort(list, low, high);
+  print(result);
+}
